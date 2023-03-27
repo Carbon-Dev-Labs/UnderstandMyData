54b0c7c113e6f1f7bce68efb80f426269f160ff8
CleverCarpet <austinjdub@outlook.com>
2023-03-27 14:30:02 -0600

    Fix menu

---
+++ assets/script/menu.js
@@ -0,0 +1,7 @@
+document.getElementById('mobileCloseButton').onclick = function () {
+    document.getElementById('mobileMenu').classList.add('hide')
+}
+
+document.getElementById('mobileOpenButton').onclick = function () {
+    document.getElementById('mobileMenu').classList.remove('hide')
+}
